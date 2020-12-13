37bf09a168240554b74fbf725c895ac754829246
danielsychoo <choointern@gmail.com>
2020-12-13 20:01:15 +0900

    Changed the Question 2
    Question2 is changed
    I don't know why But It changed 02_fibonacci

---
+++ codestates/02_fibonacci/mySolution.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+  // TODO: 여기에 코드를 작성합니다.
+}
+
+let output = fibonacci(0);
+console.log(output); // --> 0
+
+output = fibonacci(1);
+console.log(output); // --> 1
+
+output = fibonacci(5);
+console.log(output); // --> 5
+
+output = fibonacci(9);
+console.log(output); // --> 34
