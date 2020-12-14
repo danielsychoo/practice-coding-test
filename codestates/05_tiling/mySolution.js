a8e92af55e21d890cf13a31502aa7f402e921fdc
danielsychoo <choointern@gmail.com>
2020-12-15 01:11:38 +0900

    Still solve about codestates 05_tiling

--- codestates/05_tiling/mySolution.js
+++ codestates/05_tiling/mySolution.js
@@ -1,5 +1,14 @@
 let tiling = function (n) {
-  // TODO: 여기에 코드를 작성합니다.
+  // edgeCase
+  if(n === 1) {
+    return 1;
+  }
+  if(n === 2) {
+    return 2;
+  }
+
+  return (n - 2) + (n - 1);
+
 };
 
 
@@ -9,3 +18,33 @@ console.log(output); // --> 2
 
 output = tiling(4);
 console.log(output); // --> 5
+
+/*
+----------
+1 a b c d e
+2 a b c d e
+----------
+1 a a c c e
+2 b b d d e
+----------
+1 a a c d e
+2 b b c d e
+----------
+1 a b b d d
+2 a c c e e
+----------
+1 a b c c e
+2 a b d d e
+----------
+1 a b c d d
+2 a b c e e
+----------
+ */
+
+
+// 1 --> 1
+// 2 --> 2
+// 3 --> 3
+// 4 --> 5
+// 5 --> 8(?)
+
