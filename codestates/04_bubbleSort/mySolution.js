4288ad12ba0375a53785931a77032bc02ae63492
danielsychoo <choointern@gmail.com>
2020-12-14 09:53:13 +0900

    Completed Advanced about 04_bubbleSort

--- codestates/04_bubbleSort/mySolution.js
+++ codestates/04_bubbleSort/mySolution.js
@@ -3,14 +3,17 @@ const bubbleSort = function (arr) {
   let biggerEl = 0;
   let result = arr;
   for(let i = 0; i < arr.length; i++) {
+    let count = 0; // 시간단축용
     for(let n = 0; n < arr.length; n++) {
       if(arr[n] > arr[n + 1]) {
+        count++;
         smallerEl = arr[n + 1];
         biggerEl = arr[n];
         result[n] = smallerEl;
         result[n + 1] = biggerEl;
       }
     }
+    if(count === 0) return result; // 단축 실시!!!
   }
   return result;
 }
