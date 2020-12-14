a0a79da2a064da25b836dd73cb8fd893cf5c8c99
danielsychoo <choointern@gmail.com>
2020-12-14 09:45:49 +0900

    Solved 04_bubbleSort but need Advanced

--- codestates/04_bubbleSort/mySolution.js
+++ codestates/04_bubbleSort/mySolution.js
@@ -1,3 +1,20 @@
 const bubbleSort = function (arr) {
-  // TODO: 여기에 코드를 작성합니다.
-};
+  let smallerEl = 0;
+  let biggerEl = 0;
+  let result = arr;
+  for(let i = 0; i < arr.length; i++) {
+    for(let n = 0; n < arr.length; n++) {
+      if(arr[n] > arr[n + 1]) {
+        smallerEl = arr[n + 1];
+        biggerEl = arr[n];
+        result[n] = smallerEl;
+        result[n + 1] = biggerEl;
+      }
+    }
+  }
+  return result;
+}
+
+
+let output = bubbleSort([5, 4, 3, 2, 1]);
+console.log(output); // --> [1, 2, 3, 4, 5]
