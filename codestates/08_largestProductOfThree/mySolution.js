e70c37194f88c16573280af463bb733a4bf96e23
danielsychoo <choointern@gmail.com>
2020-12-20 14:07:26 +0900

    Featured about 08_largestProductOfThree

---
+++ codestates/08_largestProductOfThree/mySolution.js
@@ -0,0 +1,9 @@
+const largestProductOfThree = function (arr) {
+  
+};
+
+let output = largestProductOfThree([2, 1, 3, 7]);
+console.log(output); // --> 42 (= 2 * 3 * 7)
+
+output = largestProductOfThree([-1, 2, -5, 7]);
+console.log(output); // --> 35 (= -1 * -5 * 7)
