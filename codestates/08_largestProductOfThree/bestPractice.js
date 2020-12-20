01f7a5c2e555e92aa3ba692a69c3df548e1e0de6
danielsychoo <choointern@gmail.com>
2020-12-20 16:05:16 +0900

    Completed about CodeStates 08_largestProductOfThree

--- codestates/08_largestProductOfThree/bestPractice.js
+++ codestates/08_largestProductOfThree/bestPractice.js
@@ -1,3 +1,7 @@
 const largestProductOfThree = function (arr) {
-  // TODO: 여기에 코드를 작성합니다.
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const len = arr.length;
+  const candi1 = sorted[len - 1] * sorted[len - 2] * sorted[len - 3];
+  const candi2 = sorted[len - 1] * sorted[0] * sorted[1];
+  return Math.max(candi1, candi2);
 };
