ace74e3864ea69d6215fd253204067c4eb93cf21
danielsychoo <choointern@gmail.com>
2020-12-21 09:53:42 +0900

    Still solve Codestates 09_power

--- codestates/09_power/mySolution.js
+++ codestates/09_power/mySolution.js
@@ -1,3 +1,29 @@
 function power(base, exponent) {
-  // todo: 여기에 코드를 작성합니다.
+  let result = 1;
+  for(let i = 0; i < exponent; i++) {
+    result *= base
+    if(result > 1000000009) result %= 1000000009;
+  }
+  return result;
 }
+
+// let output = power(2, 10);
+// console.log(output); // --> 637940656
+
+let output = power(3, 40);
+console.log(output); // --> 637940656
+
+// function power(base, exponent) {
+//   let result = base;
+//   const recursive = function(base, exponent) {
+//     // baseCase
+//     if(exponent === 0) {
+//       return;
+//     }
+//     result *= base;
+//     result %= 1000000009;
+//     recursive(result, exponent - 1);
+//   }
+//   recursive(base, exponent);
+//   return result;
+// }
