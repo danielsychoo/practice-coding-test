f4308faf7016054edf3e3ea51006874a38d3dac2
danielsychoo <choointern@gmail.com>
2020-12-24 09:51:30 +0900

    Still try understand about CodeStates 11_powerSet
    I gave up sole this problem
    and I see the Reference Code
    now I'm trying to understand Reference Code

--- codestates/11_powerSet/mySolution.js
+++ codestates/11_powerSet/mySolution.js
@@ -4,30 +4,33 @@
  * 3. 빈 문자열 포함
  */
 const powerSet = function (str) {
-  let result = [''];
   let strArr = str.split('');
-  strArr.sort(); // 정렬
-  let contrastArr = strArr.slice(0, strArr.length);
-  let uniqueSortedArr = [];
+  let uniqueStrArr = Array.from(new Set(strArr)); // 중복제거
 
-  for(let i = 0; i < strArr.length; i++) {
-    if(strArr[i] !== contrastArr[i+1]) {
-      uniqueSortedArr.push(strArr[i]); // 중복제거
-    }
-  }
-
-  const recursive = function (arr, count, board) {
-    // baseCase
-    if(count === 0) {
-      result.push(board);
+  let result = [];
+  const pickOrNot = (idx, subset) => {
+    // base case
+    if (idx === uniqueStrArr.length) {
+      // 마지막 문자까지 검토한 경우
+      result.push(subset);
+      return;
     }
 
+    // recursive case
+    // idx번째 문자가 포함되지 않는 경우
+    pickOrNot(idx + 1, subset);
 
-  }
+    // idx번째 문자가 포함되는 경우
+    pickOrNot(idx + 1, subset + uniqueStrArr[idx]);
+  };
+
+  pickOrNot(0, '');
+
+  return result.sort();
 };
 
-let output1 = powerSet('abc');
-console.log(output1); // ['', 'a', 'ab', 'abc', 'ac', 'b', 'bc', 'c']
+// let output1 = powerSet('abc');
+// console.log(output1); // ['', 'a', 'ab', 'abc', 'ac', 'b', 'bc', 'c']
 
-// let output2 = powerSet('jjump');
-// console.log(output2); // ['', 'j', 'jm', 'jmp', 'jmpu', 'jmu', 'jp', 'jpu', 'ju', 'm', 'mp', 'mpu', 'mu', 'p', 'pu', 'u']
+let output2 = powerSet('jjump');
+console.log(output2); // ['', 'j', 'jm', 'jmp', 'jmpu', 'jmu', 'jp', 'jpu', 'ju', 'm', 'mp', 'mpu', 'mu', 'p', 'pu', 'u']
