3cea1fc7b42d03bc779dfd13b793e9428ce72e71
danielsychoo <choointern@gmail.com>
2020-12-29 00:37:15 +0900

    Give up solve CodeStates 12_treeBFS

--- codestates/12_treeBFS/bestPractice.js
+++ codestates/12_treeBFS/bestPractice.js
@@ -1,5 +1,16 @@
 let bfs = function (node) {
   // TODO: 여기에 코드를 작성합니다.
+  let queue = [node];
+  const values = [];
+  while (queue.length > 0) {
+    const head = queue[0];
+    queue = queue.slice(1);
+
+    values.push(head.value);
+
+    head.children.forEach((child) => queue.push(child));
+  }
+  return values;
 };
 
 // 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
@@ -14,3 +25,16 @@ Node.prototype.addChild = function (child) {
   this.children.push(child);
   return child;
 };
+
+let root = new Node(1);
+let rootChild1 = root.addChild(new Node(2));
+let rootChild2 = root.addChild(new Node(3));
+let leaf1 = rootChild1.addChild(new Node(4));
+let leaf2 = rootChild1.addChild(new Node(5));
+// let output = bfs(root);
+// console.log(output); // --> [1, 2, 3, 4, 5]
+
+leaf1.addChild(new Node(6));
+rootChild2.addChild(new Node(7));
+output = bfs(root);
+console.log(output); // --> [1, 2, 3, 4, 5, 7, 6]
